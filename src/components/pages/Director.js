882de1caab008eaec53597bdb5b0730edc8490f9
boshka0 <nastyaboksha@gmail.com>
2019-01-27 11:25:21 +0300

    feat: add map and video to the director's page

--- src/components/pages/Director.js
+++ src/components/pages/Director.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import DirectorTimeline from '../DirectorTimeline';
+import DirectorVideo from "../DirectorVideo";
+import DirectorMap from "../DirectorMap";
 
 export default function Director({ data }) {
   return (
     <div>
-      <h1>Hello</h1>
       <DirectorTimeline info={data.bio} />
+      {data.videoId && <DirectorVideo videoId={data.videoId}/>}
+      <DirectorMap city={data.city} coordinates={data.coordinates} />
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import "./App.css";
 
+import 'leaflet/dist/leaflet.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
