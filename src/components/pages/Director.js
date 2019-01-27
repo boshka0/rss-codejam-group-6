994807a3045b9b61e3c2c54db529de2c0e6104dc
boshka0 <nastyaboksha@gmail.com>
2019-01-27 11:32:31 +0300

    fix: fix the problem with director's page not starting scrolling from the top

--- src/components/pages/Director.js
+++ src/components/pages/Director.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import DirectorTimeline from '../DirectorTimeline';
 import DirectorVideo from "../DirectorVideo";
 import DirectorMap from "../DirectorMap";
 
-export default function Director({ data }) {
-  return (
-    <div>
-      <DirectorTimeline info={data.bio} />
-      {data.videoId && <DirectorVideo videoId={data.videoId}/>}
-      <DirectorMap city={data.city} coordinates={data.coordinates} />
-    </div>
-  )
+export default class Director extends Component {
+  componentDidMount() {
+    window.scrollTo(0, 0)
+  }
+  render() {
+    return (
+      <div>
+        <DirectorTimeline info={this.props.data.bio} />
+        {this.props.data.videoId && <DirectorVideo videoId={this.props.data.videoId} />}
+        <DirectorMap city={this.props.data.city} coordinates={this.props.data.coordinates} />
+      </div>
+    )
+  }
 }
-
