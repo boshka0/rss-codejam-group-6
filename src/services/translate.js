83dae402fb9657656b3f83342c3168a0d1b22813
Lesnik Matvey <qxfisher@gmail.com>
2019-01-27 14:46:54 +0300

    feat: initialize translate module

---
+++ src/services/translate.js
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import { reactI18nextModule } from "react-i18next";
+
+import translationEN from './locales/en/translation.json';
+import translationRU from './locales/ru/translation.json';
+import translationBY from './locales/by/translation.json';
+
+const resources = {
+  en: {
+    translation: translationEN
+  },
+  ru: {
+      translation: translationRU
+  },
+  by: {
+    translation: translationBY
+  }
+};
+
+i18n
+  .use(reactI18nextModule)
+  .init({
+    resources,
+    lng: "en",
+
+    keySeparator: false,
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+export default i18n;
