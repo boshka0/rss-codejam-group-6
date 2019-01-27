53f6a7495695d642582c068db702d6ae22158215
AngryIvan <leftmetalban@gmail.com>
2019-01-27 20:42:20 +0300

    feat: add gallery carousel

--- src/components/pages/Director.js
+++ src/components/pages/Director.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import DirectorTimeline from '../DirectorTimeline';
-//import SwipeGallery from 'react-swipe-gallery'
+import Slider from "react-slick";
 
 export default function Director({ data }) {
+  const settings = {
+      className: "center",
+      centerMode: true,
+      infinite: true,
+      centerPadding: "60px",
+      slidesToShow: 1,
+      speed: 500,
+      variableWidth: true
+    };
+ 
   return (
     <div>
       <div className="director-page">
@@ -13,10 +23,23 @@ export default function Director({ data }) {
           <h1 className="director-page-name">{ data.name }</h1>
           <h2 className="director-page-city">{ data.city }</h2>
         </div>
-      </div>
-      {/* <SwipeGallery elements={data["gallery"]} maxElement={3} />   */}
+        
+      </div> 
+      
+      <Slider {...settings}>
+      { data.gallery.map(photo => 
+        <div>
+          <img alt='' src={photo} className="carousel-photo"/>
+        </div>
+      )}
+      </Slider>
       <DirectorTimeline info={data.bio} />
     </div>
   )
 }
 
+// { data.gallery.map(photo => 
+//   <div>
+//     <img alt='' src={photo} className="carousel-photo"/>
+//   </div>
+// )}
