95fa32077f78e361fffa88711a4601e5e9c2edc5
boshka0 <nastyaboksha@gmail.com>
2019-01-28 22:51:30 +0300

    fix: add hash-routing instead of browser-routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Header from "./components/layout/header/Header";
 import Main from "./components/pages/Main";
