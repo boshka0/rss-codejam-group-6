f35639a38adea9de5331b68b886c27dc3bd84db0
gattygamaun <gattygamaun@gmail.com>
2019-01-28 13:45:30 +0300

    feat: change design for day of director

--- src/components/DirectorVidget.js
+++ src/components/DirectorVidget.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { withNamespaces } from 'react-i18next';
-import { Link } from "react-router-dom";
+import {withNamespaces} from 'react-i18next';
+import {Link} from "react-router-dom";
 
-function DirectorVidget({ data, lang, t }) {
+function DirectorVidget({data, lang, t}) {
   const link = `/directors/${data["id"]}`;
   return (
+    <section className="director-day">
     <div className="director-vidget">
-      <h1 className="director-vidget-heading">{t('Director of the day')}</h1>
-      <div>
-        <h3>{data.name[lang]}</h3>
-        <img src={data.gallery[0]} alt="director" />
+      <img src={data.gallery[0]} alt="director" height={300}/>
+      <div className="director-block">
+        <div className="director-vidget-heading">{t('Director of the day')}</div>
+        <div>
+          <h4>{data.name[lang]}</h4>
+        </div>
+        <Link className="director-vidget-button" to={link}>
+          {t('Learn more')}
+        </Link>
       </div>
-      <Link className="director-vidget-button" to={link}>
-        {t('Learn more')}
-      </Link>
     </div>
+    </section>
   )
 }
 
